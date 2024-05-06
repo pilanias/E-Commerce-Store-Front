09a2d6d4c01e5ac3d08ed67f3ea25835f1b09baf
Mrgnoblennon <chasebarrettbrown@hotmail.com>
2024-05-06 12:12:09 +0800

    added product display

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,19 +1,26 @@
 import getBillboard from "@/action/get-billboard";
 import Container from "@/components/ui/container"
 import Billboard from "@/components/billboard";
+import getProducts from "@/action/get-products";
+import ProductList from "@/components/product-list";
 
 export const revalidate = 0;
 
 const HomePage = async () => {
-  const billboard = await getBillboard('3a3521ea-6a6a-48a6-b959-3ca99b233a66')
+
+  const products = await getProducts({ isFeatured: true });
+  const billboard = await getBillboard('3a3521ea-6a6a-48a6-b959-3ca99b233a66');
 
   return( 
     <Container>
       <div className="space-y-10 pb-10">
         <Billboard data={billboard}/>
+        <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+          <ProductList title='Featured Products' items={products}/>
+        </div>
       </div>
     </Container>
   );
 }
  
-export default HomePage;
+export default HomePage; 
--- types.ts
+++ types.ts
@@ -9,3 +9,31 @@ export interface Category {
   name: string;
   billboard: Billboard;
 }
+
+export interface Product {
+  id: string;
+  category: Category;
+  name: string;
+  price: string;
+  isFeatured: boolean;
+  size: Size;
+  colour: Colour;
+  images: Image[];
+}
+
+export interface Image {
+  id: string;
+  url: string;
+}
+
+export interface Size {
+  id: string;
+  name: string;
+  value: string;
+}
+
+export interface Colour {
+  is: string;
+  sname: string;
+  value: string;
+}
